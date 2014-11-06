1dc5f69f7cd3635e37244fda8579bf2ae56d08f0
Geoff Pleiss <gpleiss@pivotal.io>
2014-11-06 11:28:01 -0500

    chore(javascript): setup karma
    [#80165534]
    [#81855712]

--- tasks/test.js
+++ tasks/test.js
@@ -2,12 +2,30 @@ var ejs = require('gulp-ejs');
 var errorHandler = require('./errorHandler.js');
 var fs = require('fs');
 var gulp = require('gulp');
+var karma = require('karma').server;
 var open = require('gulp-open');
 
 gulp.task('test', [
   'lint',
   '_cssCritic',
 ]);
+
+gulp.task('karma', function (done) {
+  karma.start({
+    configFile: __dirname + '/../karma.config.js',
+    browsers: ['PhantomJS'],
+    singleRun: true
+  }, done);
+});
+
+gulp.task('karma-debug', function (done) {
+  karma.start({
+    configFile: __dirname + '/../karma.config.js',
+    browsers: ['Chrome'],
+    singleRun: false,
+    autoWatch: true
+  }, done);
+});
 
 gulp.task('_copyTestAssets', ['assets'], function() {
   return gulp.src([
