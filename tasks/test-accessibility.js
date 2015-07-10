83b5c8c1fd18fa5b231490ca698ad497445bb05c
Caroline Taymor and Geoff Pleiss <ctaymor@pivotal.io>
2015-07-10 09:56:05 -0700

    WIP: Adds A11y and runs a11y accessibility tests

--- tasks/test-accessibility.js
+++ tasks/test-accessibility.js
@@ -1,5 +1,25 @@
 import gulp from 'gulp';
+import a11y from 'a11y';
+import {log, colors} from 'gulp-util';
 
-gulp.task('test-accessibility', () => {
-  throw 'This task will fail!';
+gulp.task('test-accessibility', (done) => {
+  a11y('http://styleguide-staging.cfapps.io/elements.html', (err, reports) => {
+    let hasErrors;
+
+    for (let el of reports.audit) {
+      if (el.result === 'FAIL') {
+        log(colors.red('FAIL', el.heading), el.elements.replace(/\n/g, '\n  '));
+        hasErrors = true;
+      }
+      else if (el.result === 'NA') {
+        log(colors.yellow('NA', el.heading));
+      }
+      else {
+        log(colors.green('PASS', el.heading));
+      }
+    }
+
+    if (hasErrors) { done('Google accessibility errors'); }
+    else { done(); }
+  });
 });
